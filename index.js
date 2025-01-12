06e77286380873b6760f185eb1a25ad81e3ea1ca
Josh <zylo.codes@gmail.com>
2025-01-12 08:58:50 -0600

    module syntax, attempting to insert events, but service account needs Domain-Wide Delegation of Authority

--- index.js
+++ index.js
@@ -1,32 +1,63 @@
-require('dotenv').config()
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const path = require('path')
-const cal = require('@googleapis/calendar')
+import path from 'path'
+import cal from '@googleapis/calendar'
 
-;(async () => {
-  const keyFileName = 'google-service-account-credentials.json'
-  const keyFile = path.join(__dirname, keyFileName)
-  const scopes = ['https://www.googleapis.com/auth/calendar.readonly']
+const __dirname = import.meta.dirname
+const calendarId = process.env.GOOGLE_CALENDAR_ID
 
-  const auth = new cal.auth.GoogleAuth({
-    keyFile,
-    scopes,
-  })
-  
-  const authClient = await auth.getClient()
-  
-  const client = await cal.calendar({
-    auth: authClient,
-    version: 'v3'
-  })
+const keyFileName = 'google-service-account-credentials.json'
+const keyFile = path.join(__dirname, keyFileName)
+const scopes = ['https://www.googleapis.com/auth/calendar.events']
 
-  const events = await client.events.list({
-    calendarId: 'zylo.codes@gmail.com',
-    timeMin: new Date().toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  })
-  
-  console.log(events.data.items)
-})()
+const auth = new cal.auth.GoogleAuth({
+  keyFile,
+  scopes,
+})
+
+const authClient = await auth.getClient()
+
+const client = await cal.calendar({
+  auth: authClient,
+  version: 'v3'
+})
+
+const listEvents = async () => await client.events.list({
+  calendarId,
+  timeMin: new Date().toISOString(),
+  maxResults: 10,
+  singleEvents: true,
+  orderBy: 'startTime',
+})
+
+const insertEvent = async event => await client.events.insert({
+  calendarId,
+  resource: event
+})
+
+const event = {
+  'summary': 'Test Event',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A test event',
+  'start': {
+    'dateTime': '2025-01-13T09:00:00-07:00',
+    'timeZone': 'America/Chicago',
+  },
+  'end': {
+    'dateTime': '2025-01-13T11:00:00-07:00',
+    'timeZone': 'America/Chicago',
+  },
+  'attendees': [
+    {'email': 'zylo.codes@gmail.com'},
+  ],
+  'reminders': {
+    'useDefault': true,
+  },
+};
+
+insertEvent(event)
+
+
+const events = await listEvents()  
+console.log(events.data.items)
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Google Calendar API Sandbox
+
+A quick spike on authenticating and using the Google Calendar API.
+
+* https://developers.google.com/calendar/api/guides/overview
+* https://www.geeksforgeeks.org/how-to-design-a-simple-calendar-using-javascript/
+* https://developers.google.com/identity/protocols/oauth2/scopes#calendar
+
+https://stackoverflow.com/questions/60760959/google-calendar-api-service-account-error/63715242#63715242
+https://stackoverflow.com/questions/60760959/google-calendar-api-service-account-error/63715242#63715242
