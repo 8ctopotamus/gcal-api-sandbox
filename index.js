2d9ac29049618290ccb679fd7f0b91bc3001eee9
Josh <zylo.codes@gmail.com>
2025-09-26 07:42:05 -0500

    more experimenting

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ import cal from '@googleapis/calendar'
 import creds from './google-service-account-credentials.json' assert { type: 'json' }
 
 const calendarId = process.env.GOOGLE_CALENDAR_ID
-const scopes = ['https://www.googleapis.com/auth/calendar.events']
+const scopes = ['https://www.googleapis.com/auth/calendar']
 
 const auth = new cal.auth.JWT({
   email: creds.client_email,
   key: creds.private_key,
   scopes,
-  subject: 'john@codeofthenorth.com'
+  subject: process.env.GOOGLE_SERVICE_ACCOUNT_IMPERSONATOR_EMAIL ?? undefined
 })
 
 const client = await cal.calendar({
   auth,
   version: 'v3'
 })
+
+// await auth.authorize();
 
 const listEvents = async () => await client.events.list({
   calendarId,
@@ -32,48 +34,49 @@ const insertEvent = async event => await client.events.insert({
   resource: event
 })
 
-const event = {
-  summary: 'Test Event via API',
-  location: '800 Howard St., San Francisco, CA 94103',
-  description: 'A test event',
-  start: {
-    dateTime: '2025-01-17T09:00:00-07:00',
-    timeZone: 'America/Chicago',
-  },
-  end: {
-    dateTime: '2025-01-17T11:00:00-07:00',
-    timeZone: 'America/Chicago',
-  },
-  attendees: [
-    { email: 'zylo.codes@gmail.com' },
-  ],
-  reminders: {
-    useDefault: true,
-  },
-  conferenceData: {
-    createRequest: {
-      requestId: Math.random(), 
-      conferenceSolutionKey: {
-          type: 'hangoutsMeet'
-      }
-    },
-  },
-  reminders: {
-    useDefault: false,
-    overrides: [
-      {
-        method: "email",
-        minutes: 15
-      },
-      {
-        method: "popup",
-        minutes: 15
-      }
-    ]
-  },
-};
+// const event = {
+//   summary: 'Test Event via API',
+//   location: '800 Howard St., San Francisco, CA 94103',
+//   description: 'A test event',
+//   start: {
+//     dateTime: '2025-010-17T09:00:00-07:00',
+//     timeZone: 'America/Chicago',
+//   },
+//   end: {
+//     dateTime: '2025-10-17T11:00:00-07:00',
+//     timeZone: 'America/Chicago',
+//   },
+//   attendees: [
+//     { email: 'zylo.codes@gmail.com' },
+//   ],
+//   reminders: {
+//     useDefault: true,
+//   },
+//   conferenceData: {
+//     createRequest: {
+//       requestId: Math.random(), 
+//       conferenceSolutionKey: {
+//           type: 'hangoutsMeet'
+//       }
+//     },
+//   },
+//   reminders: {
+//     useDefault: false,
+//     overrides: [
+//       {
+//         method: "email",
+//         minutes: 15
+//       },
+//       {
+//         method: "popup",
+//         minutes: 15
+//       }
+//     ]
+//   },
+// };
 
 // insertEvent(event)
 
 const events = await listEvents()  
 console.log(events.data.items)
+
